c49648cb296df7770d3663cb4edddc77773e5114
Farm <Thunderock01@gmail.com>
2024-03-27 23:32:10 +0700

    Comment error
    comment error when build

--- UnlimitedPlayers/Events/Display/MenuEvents.cs
+++ UnlimitedPlayers/Events/Display/MenuEvents.cs
@@ -20,17 +20,17 @@ namespace UnlimitedPlayers.Events.Display
 			if (!Context.IsWorldReady)
 				return;
 
-			if (Game1.IsMasterGame || Game1.IsServer)
-			{
-				// int buildingsConstructed = Game1.getFarm().getNumberBuildingsConstructed("Cabin");
-				Type type = typeof(CarpenterMenu);
-				object newMenu = e.NewMenu;
+			// if (Game1.IsMasterGame || Game1.IsServer)
+			// {
+			// 	// int buildingsConstructed = Game1.getFarm().getNumberBuildingsConstructed("Cabin");
+			// 	Type type = typeof(CarpenterMenu);
+			// 	object newMenu = e.NewMenu;
 
-				List<BluePrint> bluePrints = LazyHelper.GetInstanceField(type, newMenu, "blueprints") as List<BluePrint>;
-				bluePrints?.Add(new BluePrint("Stone Cabin"));
-				bluePrints?.Add(new BluePrint("Plank Cabin"));
-				bluePrints?.Add(new BluePrint("Log Cabin"));
-			}
+			// 	List<BluePrint> bluePrints = LazyHelper.GetInstanceField(type, newMenu, "blueprints") as List<BluePrint>;
+			// 	bluePrints?.Add(new BluePrint("Stone Cabin"));
+			// 	bluePrints?.Add(new BluePrint("Plank Cabin"));
+			// 	bluePrints?.Add(new BluePrint("Log Cabin"));
+			// }
 		}
 	}
 }
